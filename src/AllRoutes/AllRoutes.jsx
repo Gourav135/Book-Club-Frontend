0650447f646818c4325dc3f801519e99b323f995
sreerag-rajan <85482640+sreerag-rajan@users.noreply.github.com>
2022-04-22 20:53:05 +0530

    login and register built along with redux

--- src/AllRoutes/AllRoutes.jsx
+++ src/AllRoutes/AllRoutes.jsx
@@ -3,11 +3,15 @@ import {Home} from "../components/Home/Home"
 import {Books} from "../components/Books/Books"
 import { UserRoom } from "../components/UserRoom/UserRoom";
 import { AllRoom } from "../components/AllRoom/AllRoom";
+import { Register } from "../components/Register/Register";
+import { Login } from "../components/Login/Login";
 
 export const AllRoutes = () => {
     return (
         <Routes>
             <Route path="/" element={<Home/>}/>
+            <Route path="/login" element={<Login/>}/>
+            <Route path="/register" element={<Register/>}/>
             <Route path="/Books" element={<Books/>}/>
             <Route path="/UserRoom" element={<UserRoom/>}/>
             <Route path="/AllRoom" element={<AllRoom/>}/>
