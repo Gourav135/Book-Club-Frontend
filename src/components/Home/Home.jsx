cd59b723029407f0ff2c5a2559d538d7eb8a8366
sreerag-rajan <85482640+sreerag-rajan@users.noreply.github.com>
2022-04-23 02:04:58 +0530

    a temporary home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,43 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 
 
 
 export const Home = () => {
+    const [books, setBooks] = useState([]);
+    useEffect(()=>{
+        axios.get(`http://openlibrary.org/subjects/love.json?limit=10`).then(({data})=>{
+            console.log(data)
+            let x = data.works.map((el)=>{
+                // console.log(el)
+                return {
+                    title:el.title,
+                    author:el.authors[0].name,
+                    cover:`https://covers.openlibrary.org/b/id/${el.cover_id}-M.jpg`,
+                    key:el.key
+                }
+            })
+            console.log(x)
+            setBooks(x);
+        }).catch((er)=>{
+            console.log(er)
+        })
+    },[])
     return (
         <div>
-            Home
+            {books.length>0&&books.map((el)=>{
+                return <div key={el.key}>
+                    <Link to={`${el.key.trim().split("/")[2]}`}>
+                    <img src={el.cover} alt="cover" />
+                    <h3>{el.title}</h3>
+                    <p>{el.author&&el.author[0]}</p>
+                    </Link>
+                </div>
+            })}
         </div>
     )
 }
+
+//http://openlibrary.org/search.json?q=the+lord+of+the+rings
